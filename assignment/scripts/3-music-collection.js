0734d60080ea7e5e5a26d0ef200972062b4b9c5b
alimici2016 <78386559+alimici2016@users.noreply.github.com>
2021-09-25 18:20:30 -0500

    Update 3-music-collection.js

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -5,35 +5,33 @@ let collection = []
       collection.push(record)
       return record;
   }
-//  addToCollection('Blonde', 'Frank Ocean', 2016);
- console.log(addToCollection('Blonde', 'Frank Ocean', 2016));
-  //addToCollection('Nothing Was the Same', 'Drake', 2013);
+
+  console.log(addToCollection('Blonde', 'Frank Ocean', 2016));
   console.log(  addToCollection('Nothing Was the Same', 'Drake', 2013));
-  //addToCollection('Victory Lap', 'Nipsey Hussle', 2018);
-    console.log(addToCollection('Victory Lap', 'Nipsey Hussle', 2018));
-//  addToCollection('My Beautiful Dark Twisted Fantasy', 'Kanye West', 2010)
+  console.log(addToCollection('Victory Lap', 'Nipsey Hussle', 2018));
   console.log(addToCollection('My Beautiful Dark Twisted Fantasy', 'Kanye West', 2010));
-//  addToCollection('Trip', 'Jhene Aiko', 2017);
   console.log(addToCollection('Trip', 'Jhene Aiko', 2017));
-//  addToCollection('Enter the Wu-Tang 36 chambers', 'Wu-Tang Clan', 1993);
   console.log(addToCollection('Enter the Wu-Tang 36 chambers', 'Wu-Tang Clan', 1993));
-console.log(collection);
+  console.log(collection);
+
+//let animals = ['cat','dog', 'mouse']
+//Testing to check if the code below was generic.
 
 function showCollection(array){
-  console.log(collection.length);
-    for (album of collection){
+  console.log('albums in collection', array.length);
+    for (album of array){
       console.log(album);
   }
 }
+showCollection(collection);
 
-showCollection();
 function findByArtist(artist){
   let results = []
-    for (artist of collection){
-      if (artist === collection.artist){
-        result.push(artist)
+    for (let person of collection){
+      if (person.artist === artist){
+        results.push(person)
       }
-  }
+    }
       return results;
   }
-findByArtist();
+console.log(findByArtist('Wu-Tang Clan'));
